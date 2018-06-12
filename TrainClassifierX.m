98ed49480bdbbd6dc3c1352013bf0036e4a6cb68
Marcin <33329142+mlaskowski17@users.noreply.github.com>
2018-06-12 16:16:50 +0100

    Update TrainClassifierX.m

--- TrainClassifierX.m
+++ TrainClassifierX.m
@@ -1,9 +1,6 @@
 
 function parameters = TrainClassifierX(input,output)
 
-%% All implementation should be inside the function.
-
-% For example: Method not using any parameters.
 parameters = [input, output];
 
 % input: train_data and train_labels
