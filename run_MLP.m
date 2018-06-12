14af63fb04cc5b77ab39687897bf523d71bbb0ab
Marcin <33329142+mlaskowski17@users.noreply.github.com>
2018-06-12 16:16:13 +0100

    Update run_MLP.m

--- run_MLP.m
+++ run_MLP.m
@@ -37,40 +37,17 @@ nbrOfNeuronsInEachHiddenLayer = [5];
 % Default number of epochs: 500.
 nbrOfEpochs_max = 500;
 
-%% Question 1A - How does the learning rate influence the training process?
-% This is classic implementation of the backpropagation-based learning
-% algorithm. All the methods in the function MLP_1A are covered in the
-% lecture.
-% Hint: You may have noticted that the learning is relatviely slow. You can
-% try to run the code with more epochs to see at which epoch do the learning
-% curves plateau. DON'T spend too much time on such experiments, as the
-% process can be very time consuming. In the end you should report the
-% results for 500 epochs.
-
 % Learning rate
 % Default learning rate: 0.0001
 learningRate = 0.0001;
 
-% Should learning rate decrease with each epoch?
 enable_decrease_learningRate = 0; %1 for enable decreasing, 0 for disable
 learningRate_decreaseValue = 0.0000001; % decrease value
 min_learningRate = 0.00005; % minimum learning rate
 
 [accuracy_1a, best_prediction_1a] = MLP_1A(train_data, train_labels, test_data, test_labels, nbrOfNeuronsInEachHiddenLayer, learningRate, nbrOfEpochs_max, enable_decrease_learningRate, learningRate_decreaseValue, min_learningRate);
 
-% accuracy_1a - [nbrOfEpochs_max x 1] vector of accuracies obtained for each
-% of training epochs. So-called 'learning curve'.
-% 
-% best_prediction_1a - [number of datapoints x 1] vector of predicted classes
-% for each datapoint for the epoch that yielded the best accuracy. This can
-% be directly compared with target_labels containing the true labels.
 
-%% Rest of the questions
-% Note that this function does not take learning rate as an input, as the 
-% resilient gradient descent backpropagation (RPROP) is used in the training
-% process. The reason for using it is better overall perfomance and much 
-% faster runtimes.
-% 
 % The original paper describing the method:
 % Martin Riedmiller, 'Rprop -Description and Implementation Details', 
 % Technical Report, January 1994
